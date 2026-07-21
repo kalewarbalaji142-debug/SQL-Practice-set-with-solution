1e8be287dcdeb38cfb067359657b5cdf7f4405d9
Ritesh Kalewar <kalewarbalaji142@gmail.com>
2026-07-21 13:25:09 +0530

    Add files via upload

---
+++ 7.sql
@@ -0,0 +1,8 @@
+-- Detrmine the distribution of orders by hour of the day.
+
+SELECT 
+    HOUR(order_time), COUNT(order_id) AS order_count
+FROM
+    orders
+GROUP BY HOUR(order_time)
+ORDER BY HOUR(order_time);
